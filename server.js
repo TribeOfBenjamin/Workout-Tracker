964051bc5de26273db8d58e0c4a6646d290bc3ff
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-07-27 21:47:22 -0500

    fixed mongoose require

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 // Requiring necessary npm packages
 const express = require("express");
 const exphbs = require("express-handlebars");
-const { Mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 
 // Creating express app
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -18,6 +20,8 @@ app.set("view engine", "handlebars");
 // Setting up database
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://TribeOfBenjamin:NewYork1991@ds339648.mlab.com:39648/heroku_88k4jhj3";
 mongoose.connect(MONGODB_URI);
+
+// Put routes here (for later)
 
 app.listen(PORT, () => {
     console.log(
